00e09483389ec99cdb27134b7e3a47f53b9ca144
Teanna <teannamtaylor95@gmail.com>
2023-05-13 09:43:00 -0700

    commit tree 59721333542c6ce0f10670d8c8c85c5a7dd9fc0e

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,18 @@
-def oxford_comma(array)
 
+# array of string elements to a string using the oxford comma
+def oxford_comma(array)
+    result = []
+    last = array[-1]
+    case array.length
+    when 1
+        array[0]
+    when 2
+        array.join(" and ")
+    else
+        result = array.take(array.length - 1)
+        result << "and "
+        return_arr = result.join(", ")
+        return_arr.split(" ")
+        return_arr << last
+    end
 end
